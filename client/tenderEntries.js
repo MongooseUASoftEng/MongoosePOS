812cc76a029e1e4be520bfe75f552ef50199771a
aepohl <aepohl@uark.edu>
2015-09-29 13:55:55 -0500

    Uncomment now that template is in place

--- client/tenderEntries.js
+++ client/tenderEntries.js
@@ -1,7 +1,7 @@
 // This template doesn't exist yet. It was causing an exception in the browser.
 
-// Template.tenderEntries.helpers({
-//   tenderEntries: function () {
-//     return TenderEntries.find();
-//   }
-// });
+ Template.tenderEntries.helpers({
+   tenderEntries: function () {
+     return TenderEntries.find();
+   }
+ });
