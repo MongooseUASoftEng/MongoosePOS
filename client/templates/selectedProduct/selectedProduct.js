babd286ba103c28703889f393406509ab9c3e4e9
Henry Darnell <hjdarnel@uark.edu>
2015-11-02 12:17:18 -0600

    Moved currentTransactionEntries into its own folder

--- client/templates/selectedProduct/selectedProduct.js
+++ client/templates/selectedProduct/selectedProduct.js
@@ -4,25 +4,11 @@ Template.selectedProduct.helpers({
   },
 });
 
-Template.currentTransactionEntries.helpers({
-  currentTransactionEntries: function() {
-    return TransactionEntries.find();
-  }
-})
-
 Template.selectedProduct.onCreated(function() {
   var self = this;
   // Subscribe to the data
   self.autorun(function() {
     var searchedCode = Number(FlowRouter.getParam('searchedCode'));
     self.subscribe('selectedProduct', searchedCode);
-  });
-});
-
-Template.currentTransactionEntries.onCreated(function() {
-  var self = this;
-  // Subscribe to the data
-  self.autorun(function() {
-    self.subscribe('currentTransactionEntries');
   });
 });
