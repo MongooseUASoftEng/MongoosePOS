46b680e452186218c361749f72f9c271717d02a3
Will Poorman <will.k.poorman@gmail.com>
2015-11-17 13:05:34 -0600

    Add buttons and quantity input to selectedProduct page

--- client/templates/selectedProduct/selectedProduct.js
+++ client/templates/selectedProduct/selectedProduct.js
@@ -4,6 +4,24 @@ Template.selectedProduct.helpers({
   },
 });
 
+Template.selectedProduct.events({
+  "submit  #addingAmount": function(event, template){
+    var addAmmountJS = document.getElementById('addAmount').value;
+    var addAmount = $('#addAmount').val();
+
+    if (!addAmount) {
+      alert('Enter an ammount to add!');
+    } else {
+      alert('Added ' + addAmount);
+    }
+
+    return false;
+  },
+  "click  #cancel": function(event, template) {
+    alert("Canceled");
+  }
+});
+
 Template.selectedProduct.onCreated(function() {
   var self = this;
   // Subscribe to the data
