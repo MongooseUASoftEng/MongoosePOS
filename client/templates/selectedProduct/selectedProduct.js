bd92de707eff45bc08dd355e1f4ce776b207ad70
Isaac Snow <isaacjsnow@gmail.com>
2015-12-07 14:35:12 -0600

    Save current transactions to session

--- client/templates/selectedProduct/selectedProduct.js
+++ client/templates/selectedProduct/selectedProduct.js
@@ -12,3 +12,28 @@ Template.selectedProduct.onCreated(function() {
     self.subscribe('selectedProduct', searchedCode);
   });
 });
+
+Template.selectedProduct.onRendered(function () {
+  // Auto focus on the submit button so you can just hit enter to submit.
+  $('#addProduct').focus();
+});
+
+Template.selectedProduct.events({
+  "submit #addProductForm": function(event, template){
+    var code = getSelectedCode();
+
+    if (Session.get('currentTransactionEntries') === undefined) {
+      Session.set('currentTransactionEntries', [ code ]);
+    } else {
+      // Get the current entries, copy the array, add the new value to it.
+      var entries = Session.get('currentTransactionEntries').slice().concat(code);
+      Session.set('currentTransactionEntries', entries);
+    }
+
+    return false;
+  }
+});
+
+function getSelectedCode () {
+  return 710779560208;
+}
