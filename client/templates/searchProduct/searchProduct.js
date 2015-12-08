6cc06aca1647e3e5a67101f0a7b7a89cb30ac729
Henry Darnell <hjdarnel@uark.edu>
2015-12-07 18:04:43 -0600

    Working collapsed-open div for selected product

--- client/templates/searchProduct/searchProduct.js
+++ client/templates/searchProduct/searchProduct.js
@@ -1,6 +1,6 @@
 Template.searchProduct.events({
   "submit #searchingCode": function(event, template){
-    var searchedCode = $('#searchedCode').val();
+    var searchedCode = parseInt($('#searchedCode').val(), 10);
 
     if (!searchedCode) {
       alert('Enter an item\'s look up code!');
@@ -8,7 +8,7 @@ Template.searchProduct.events({
     }
 
     Session.set("searchedCode", searchedCode);
-    document.getElementById("selectedProductContainer").style.height='300px';
+    document.getElementById("selectedProductContainer").style.height='500px';
     Blaze.render(Blaze.Template.selectedProduct, $('#selectedProductContainer').get(0)); //loads div
 
 
